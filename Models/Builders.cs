7e370cdb870012b81b9ac20907b666df348f9988
TheGreatRy <158355076+TheGreatRy@users.noreply.github.com>
2026-05-18 20:26:35 -0600

    Builder Pattern with Shapes
    Applied the builder pattern to allow the system to create a shape from a set of points
    
    This will be used to create shapes based on mouse clicks

---
+++ Models/Builders.cs
@@ -0,0 +1,152 @@
+﻿using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace ExpandedDimensions.Models
+{
+    //Allows different types of shapes to be drawn
+    public class Builder
+    {
+        //The drawing object to reference
+        protected Drawing _drawing = new Drawing();
+        //Default Constructor: internally sets the max amount of points
+        public Builder() { SetMaxPoints(); }
+        //Sets the max amount of points
+        protected virtual void SetMaxPoints() { }
+        //Add a point to the Drawing points array
+        public void AddPoint(double x, double y) { _drawing.AddPoint(x, y); }
+        //The resulting Shape from the Builder
+        public virtual Shape Draw() { return null; }
+    }
+    public class LineBuilder : Builder
+    {
+        //Force max to be 2
+        protected override void SetMaxPoints()
+        {
+            _drawing.SetMaxPoints(2);
+        }
+        //Creates a Line object from the points array
+        public override Shape Draw()
+        {
+            if (_drawing.GetPointsLength() > 1)
+            {
+                Line drawing = new Line();
+                drawing.X1 = _drawing.GetPointsList()[0].X;
+                drawing.Y1 = _drawing.GetPointsList()[0].Y;
+                drawing.X2 = _drawing.GetPointsList()[1].X;
+                drawing.Y2 = _drawing.GetPointsList()[1].Y;
+
+                return drawing;
+            }
+            return null;
+        }
+    }
+
+    public class EllipseBuilder : Builder
+    {
+        //Force max to be 2
+        protected override void SetMaxPoints()
+        {
+            _drawing.SetMaxPoints(2);
+        }
+        //Creates an Ellipse object from the points array
+        public override Shape Draw()
+        {
+            if (_drawing.GetPointsLength() > 1)
+            {
+                Ellipse drawing = new Ellipse();
+                double width = Math.Abs((_drawing.GetPointsList()[0].X - _drawing.GetPointsList()[1].X));
+                double height = Math.Abs((_drawing.GetPointsList()[0].Y - _drawing.GetPointsList()[1].Y));
+
+                drawing.Width = width;
+                drawing.Height = height;
+
+                return drawing;
+            }
+            return null;
+        }
+    }
+
+    public class PolygonBuilder : Builder
+    {
+        //Allow *infinite* amount of points
+        protected override void SetMaxPoints()
+        {
+            _drawing.SetMaxPoints(int.MaxValue);
+        }
+        //Creates a Polygon object from the points array
+        public override Shape Draw()
+        {
+            if (_drawing.GetPointsLength() > 1)
+            {
+                Polygon drawing = new Polygon();
+                PointCollection points = new PointCollection();
+
+                for (int i = 0; i < _drawing.GetPointsList().Count; i++)
+                {
+                   points.Add(_drawing.GetPointsList()[i]);
+                }
+
+                //Polygon needs to be a closed drawing
+                points.Add(_drawing.GetPointsList()[0]);
+
+                drawing.Points = points;
+
+                return drawing;
+            }
+            return null;
+        }
+    }
+
+    public class PolylineBuilder : Builder
+    {
+        //Allow *infinite* amount of points
+        protected override void SetMaxPoints()
+        {
+            _drawing.SetMaxPoints(int.MaxValue);
+        }
+        //Creates a Polygon object from the points array
+        public override Shape Draw()
+        {
+            if (_drawing.GetPointsLength() > 1)
+            {
+                Polygon drawing = new Polygon();
+                PointCollection points = new PointCollection();
+
+                for (int i = 0; i < _drawing.GetPointsList().Count; i++)
+                {
+                    points.Add(_drawing.GetPointsList()[i]);
+                }
+
+                drawing.Points = points;
+
+                return drawing;
+            }
+            return null;
+        }
+    }
+
+    public class RectangleBuilder : Builder
+    {
+        //Force max to be 2
+        protected override void SetMaxPoints()
+        {
+            _drawing.SetMaxPoints(2);
+        }
+        //Creates a Rectangle object from the points array
+        public override Shape Draw()
+        {
+            if (_drawing.GetPointsLength() > 1)
+            {
+                Rectangle drawing = new Rectangle();
+                double width = Math.Abs((_drawing.GetPointsList()[0].X - _drawing.GetPointsList()[1].X));
+                double height = Math.Abs((_drawing.GetPointsList()[0].Y - _drawing.GetPointsList()[1].Y));
+
+                drawing.Width = width;
+                drawing.Height = height;
+
+                return drawing;
+            }
+            return null;
+        }
+    }
+}
---
+++ Models/Point.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace ExpandedDimensions.Models
+{
+    public class Drawing
+    {
+        //Variables
+        private int _maxPoints = 2;
+        private List<Point> _points = new List<Point>();
+        
+        //Check to see if the amount of points exceeds the limit
+        public bool GetMaxCheck()
+        {
+            return _maxPoints >= _points.Count;
+        }
+        //Get the current count of the point array for comparisons
+        public int GetPointsLength()
+        { 
+            return _points.Count; 
+        }
+        //Get the array of points being stored
+        public List<Point> GetPointsList()
+        {
+            return _points;
+        }
+        //Set the max amount of points a Drawing can have
+        public void SetMaxPoints(int maxPoints)
+        {
+            _maxPoints = maxPoints;
+        }
+        //Add a point to the array for the Drawing
+        public void AddPoint(double x, double y)
+        {
+            if (GetMaxCheck()) _points.Clear();
+            else _points.Add(new Point(x, y));
+        }
+    }
+}
